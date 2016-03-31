0734c4c62485f2b4cefb7591735ca73516de9629
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2016-03-31 18:14:58 -0300

    Created specs to Student's models validations.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:register_number) }
+    it { should validate_presence_of(:status) }
+    it { should validate_uniqueness_of(:register_number) }
+  end
 end
