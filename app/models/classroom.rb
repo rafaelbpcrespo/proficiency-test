8c2c46af524f41dc4a457b276eae329d3c5a2011
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2016-03-31 19:27:23 -0300

    Adding the associations on the Classroom model and created a before_create method to set the entry_at value with the current time.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,2 +1,12 @@
 class Classroom < ActiveRecord::Base
+  belongs_to :student
+  belongs_to :course
+
+  before_create :set_entry_at
+  validates :student_id, :course_id, presence: true
+
+  def set_entry_at
+    self.entry_at = DateTime.now
+  end
+
 end
--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Classroom, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:student_id) }
+    it { should validate_presence_of(:course_id) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:student) }
+    it { is_expected.to belong_to(:course) }
+  end
+
+  describe 'before_create' do
+    describe '#set_entry_at' do
+      let(:course) { create :course }
+      let(:student) { create :student }
+
+      it 'should have the entry_at value equal the current time of the register' do
+        classroom = Classroom.create(student: student, course: course)
+
+        expect(classroom.entry_at.strftime("%d/%m/%Y %H:%M")).to eq DateTime.now.strftime("%d/%m/%Y %H:%M")
+      end
+    end
+  end
 end
